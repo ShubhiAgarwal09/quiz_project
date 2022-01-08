f624b6aa2e0fb8513d26ff22b4743792343d5147
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2022-01-08 11:10:24 +0530

    commit

--- student/ajax/activetest.php
+++ student/ajax/activetest.php
@@ -1,15 +1,15 @@
 <?php
 include('connection.php');
 session_start();
-// $today = date("Y-m-d");
-// $examdate = date("2022-01-02");
-//if ($today == $examdate) {
+//  $today = date("Y-m-d");
+//  $examdate = date("2022-01-02");
+// if ($today == $examdate) {
   $testId = $_POST['activeTest'];
   $_SESSION['activeTest'] = $testId;
   echo 0;
-// }// else {
-//   echo 1;
-// }
+//  } else {
+//    echo 1;
+//  }
 
 function test_input($data)
 {
--- student/testpage.php
+++ student/testpage.php
@@ -176,11 +176,11 @@ function createDivForQuestion(data) {
             <input type="radio" id="option1" name="options" value="option1">
             <label for="option1">${data[questionNumber].option1}</label><br>
             <input type="radio" id="option2" name="options" value="option2">
-            <label for="css">${data[questionNumber].option2}</label><br>
+            <label for="option2">${data[questionNumber].option2}</label><br>
             <input type="radio" id="option3" name="options" value="option3">
-            <label for="javascript">${data[questionNumber].option3}</label> <br>
+            <label for="option3">${data[questionNumber].option3}</label> <br>
             <input type="radio" id="option4" name="options" value="option4">
-            <label for="javascript">${data[questionNumber].option4}</label>  
+            <label for="option4">${data[questionNumber].option4}</label>  
         </div>`);
 }
 
